1db7bbe4bd6677d80aabc94ec95751e30073f709
giuliomarinelli <giulio.marinelli@icloud.com>
2024-02-23 11:29:48 +0100

    auth service implementation complete so far, need to implement spring security  classes nd then continueÂ

---
+++ src/main/java/it/epicode/w7d5/event_management/exceptions/BadRequestException.java
@@ -0,0 +1,7 @@
+package it.epicode.w7d5.event_management.exceptions;
+
+public class BadRequestException extends Exception {
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
--- src/main/java/it/epicode/w7d5/event_management/repositories/UserRepository.java
+++ src/main/java/it/epicode/w7d5/event_management/repositories/UserRepository.java
@@ -2,10 +2,16 @@ package it.epicode.w7d5.event_management.repositories;
 
 import it.epicode.w7d5.event_management.Models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
+    public Optional<User> findByEmail(String email);
+    @Query("SELECT u.email FROM User u")
+    public List<String> getAllEmails();
 }
